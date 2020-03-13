134c4e9db459b692316469e69bce6c130ab8a1be
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-03-13 13:57:29 +0300

    Studying Spring (13.03.2020)

--- src/main/java/com/springlessons/lesson1/TestSpring.java
+++ src/main/java/com/springlessons/lesson1/TestSpring.java
@@ -5,12 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
 
     public static void main(String[] args) {
-
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
         MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
 
         player.playMusicList();
+        System.out.println(player.getPlayerName());
+        System.out.println(player.getVolume());
 
     }
 }
