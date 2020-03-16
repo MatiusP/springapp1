3e251c11bd37495aeae56351cfb7946d743f51f5
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-03-16 17:13:57 +0300

    Studying Spring (16.03.2020)

--- src/main/java/com/springlessons/lesson1/TestSpring.java
+++ src/main/java/com/springlessons/lesson1/TestSpring.java
@@ -6,11 +6,21 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        player.playMusicList();
-        System.out.println(player.getPlayerName());
-        System.out.println(player.getVolume());
+        boolean io = player1 == player2;
+        System.out.println(io);
+
+        System.out.println(player1);
+        System.out.println(player2);
+
+        player1.setVolume(66);
+        System.out.println(player1.getVolume());
+        System.out.println(player2.getVolume());
+//        player.playMusicList();
+//        System.out.println(player.getPlayerName());
+//        System.out.println(player.getVolume());
 
     }
 }
