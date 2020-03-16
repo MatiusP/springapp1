15c95cf94713b874ecca3c95f09adaa3c354edd3
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-03-16 22:33:15 +0300

    Studying Spring (16.03.2020)

--- src/main/java/com/springlessons/lesson2/MusicPlayer.java
+++ src/main/java/com/springlessons/lesson2/MusicPlayer.java
@@ -1,21 +1,22 @@
 package com.springlessons.lesson2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicMusic classicMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicMusic classicMusic, RockMusic rockMusic) {
-        this.classicMusic = classicMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     String playMusic() {
-        return "Now playing: " + classicMusic.getSong();
+        return "Now playing: " + music1.getSong() + ", " + music2.getSong();
 //        System.out.println("Now playing: " + rockMusic.getSong());
 
     }
