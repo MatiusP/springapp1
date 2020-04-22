95496784caf82f73ed1f1ac8baf3304096c19dda
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-04-22 11:52:31 +0300

    Studying Spring (18/03/2020)

---
+++ src/main/java/com/springlessons/lesson6/Other.java
@@ -0,0 +1,13 @@
+package com.springlessons.lesson6;
+
+
+public class Other {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.print(i);
+        i = i++;
+        System.out.println(i);
+
+    }
+}
